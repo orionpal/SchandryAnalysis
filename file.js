5ebeab541fe7fa8675138d6f55805626065a0769
orionpal <orionpal@buffalo.edu>
2020-03-31 00:03:52 -0400

    basic listing of words

--- file.js
+++ file.js
@@ -1,4 +1,6 @@
+var words = {};
 document.getElementById('file').onchange = function(){
+  words = {};
   //get the file that was selected
   var file = this.files[0];
   //create a new file reader
@@ -7,8 +9,8 @@ document.getElementById('file').onchange = function(){
   reader.onload = function(progressEvent){
     // split the file by lines
     var lines = this.result.split('\n');
-    // do something with the lines, currently only reads the first 5 entries, strong recommend against printing every single entry
-    for(var line = 0; line < 5; line++){
+    // go through lines and count words (saved in words)
+    for(var line = 0; line < 2; line++){
       var entry = lines[line]; //get specified entry on line "line"
       var i = 0;
       while (i<entry.length){
@@ -18,8 +20,27 @@ document.getElementById('file').onchange = function(){
           while (j<entry.length){
             //find where the tag "SN" is after the abstract (\t is the tabulation character)
             if (entry.substring(j,j+4)=='"\tSN'){
-              //print only the abstract
-              document.getElementById("content").innerHTML += entry.substring(i+3, j)+"<br><br>";
+              entry = entry.substring(i+3, j);
+              var word = '';
+              //go through entry to find instances of words
+              for (var chari=0; chari<entry.length; chari++){
+                var char = entry[chari].toLowerCase(); //standardize letter to lowercase
+                //if the char is a letter then add it to the word
+                if (char != char.toUpperCase()){
+                  word += char;
+                }//if the char is a symbol then check if the word is longer than 2 letters
+                else if (word.length>2){
+                  if (word in words){
+                    words[word] += 1;
+                  }else{
+                    words[word] = 1;
+                  }
+                  word = '';
+                }//otherwise our word is too small/insignificant
+                else{
+                  word = '';
+                }
+              }
               break;
             }
             j += 1
@@ -29,6 +50,17 @@ document.getElementById('file').onchange = function(){
         i += 1;
       }
     }
+    //show word counts in alphabetical order
+    console.log(words)
+    var justWords = Object.keys(words);
+    justWords.sort();
+
+    var word = document.getElementById("word");
+    var count = document.getElementById("count");
+    for (var i=0; i<justWords.length; i++){
+      word.innerHTML += `${justWords[i]}<br>`
+      count.innerHTML += `${words[justWords[i]]}<br>`
+    }
   };
-  reader.readAsText(file);
+  reader.readAsText(file); //read file and do function previously defined
 };
